3ad75bd5404935b42159134f6e02c1b11e4fe07d
Latish Sehgal <latish.sehgal@gmail.com>
2011-01-05 16:33:39 -0600

    Added empty unit tests

---
+++ FormatAppSettings/given_an_appsettings_file.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FormatAppSettings
+{
+    [TestClass]
+    class given_an_appsettings_file
+    {
+        [TestMethod]
+        public void it_should_sort_elements_by_key_value()
+        {
+
+        }
+
+        [TestMethod]
+        public void it_should_process_elements_at_multiple_levels_and_maintain_hierarchy()
+        {
+
+        }
+
+        [TestMethod]
+        public void it_should_preserve_comments_associated_with_elements()
+        {
+
+        }
+
+        [TestMethod]
+        public void it_should_preserve_hard_returns_associated_with_elements()
+        {
+
+        }
+
+        [TestMethod]
+        public void it_should_compress_multiple_hard_returns()
+        {
+
+        }
+    }
+}

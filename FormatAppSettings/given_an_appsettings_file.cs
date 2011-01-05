84ff5c655354f4311879fbf224ffb19ba3b02598
Latish Sehgal <latish.sehgal@gmail.com>
2011-01-05 17:20:33 -0600

    End of day 1

---
+++ FormatAppSettings/given_an_appsettings_file.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FormatAppSettings
+{
+    [TestClass]
+    public class given_an_appsettings_file
+    {
+        [TestMethod]
+        public void it_should_sort_elements_by_key_value()
+        {
+            Assert.Fail("Yet to be implemented.");
+        }
+
+        [TestMethod]
+        public void it_should_process_elements_at_multiple_levels_and_maintain_hierarchy()
+        {
+            Assert.Fail("Yet to be implemented.");
+        }
+
+        [TestMethod]
+        public void it_should_preserve_comments_associated_with_elements()
+        {
+            Assert.Fail("Yet to be implemented.");
+        }
+
+        [TestMethod]
+        public void it_should_preserve_hard_returns_associated_with_elements()
+        {
+
+            Assert.Fail("Yet to be implemented.");
+        }
+
+        [TestMethod]
+        public void it_should_compress_multiple_hard_returns()
+        {
+            Assert.Fail("Yet to be implemented.");
+        }
+    }
+}

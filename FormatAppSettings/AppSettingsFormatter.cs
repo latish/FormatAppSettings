f2ab07f5379186d342db2473c18358304baa6fd5
Latish Sehgal <latish.sehgal@gmail.com>
2011-01-07 17:19:40 -0600

    Yay! 2 passing unit tests. We are on our way.

---
+++ FormatAppSettings/AppSettingsFormatter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace FormatAppSettings
+{
+    public class AppSettingsFormatter
+    {
+        public string Tidy(string inputXml)
+        {
+            var xmlDoc = XDocument.Parse(inputXml,LoadOptions.PreserveWhitespace);
+            var elements = xmlDoc.Elements().First().Elements();
+            elements = elements.OrderBy(element => (string)element.Attribute("env")).OrderBy(element => (string)element.Attribute("key")).ToList();
+            xmlDoc.Elements().First().Elements().Remove();
+            xmlDoc.Elements().First().Add(elements);
+
+            return xmlDoc.Declaration + xmlDoc.ToString(SaveOptions.DisableFormatting);
+        }
+    }
+}
